f8f83798f6be4345df82595d9e7e681bef7291d5
Arthur Gunn <arthur@gunn.co.nz>
2011-03-07 00:54:17 +1300

    Created a middleman project with the activo demo in haml form.

---
+++ config.rb
@@ -0,0 +1,60 @@
+Sinatra::Base.mime_type :woff, "application/x-font-woff"
+
+# Automatic sitemaps
+# activate :slickmap
+
+# CodeRay syntax highlighting in Haml
+# activate :code_ray
+
+# Automatic image dimension calculations
+# activate :automatic_image_sizes
+
+# Per-page layout changes
+# With no layout
+# page "/path/to/file.html", :layout => false
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+
+# Helpers
+helpers do
+  def some_helper(*args)
+    "Helping"
+  end
+end
+
+
+# Change the CSS directory
+# set :css_dir, "alternative_css_directory"
+
+
+
+# Change the JS directory
+# set :js_dir, "alternative_js_directory"
+
+
+
+# Change the images directory
+# set :images_dir, "alternative_image_directory"
+
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Shrink/smush PNG/JPEGs on build
+  # activate :smush_pngs
+  
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Generate ugly/obfuscated HTML from Haml
+  # activate :ugly_haml
+
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
